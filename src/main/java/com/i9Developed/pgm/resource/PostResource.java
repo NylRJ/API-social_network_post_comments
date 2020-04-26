7216c6b9d544ffb915e593b0562b819546720b04
Moises <loveloungeoficial@gmail.com>
2020-04-26 03:50:22 -0300

    Obtendo um post por id

--- src/main/java/com/i9Developed/pgm/resource/PostResource.java
+++ src/main/java/com/i9Developed/pgm/resource/PostResource.java
@@ -1,15 +1,22 @@
 package com.i9Developed.pgm.resource;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.i9Developed.pgm.domain.Post;
+import com.i9Developed.pgm.services.PostService;
 
 @RestController
 @RequestMapping(value = "/posts")
 public class PostResource {
-//
-//	@Autowired
-//	private PostService PostService;
-//
+
+	@Autowired
+	private PostService PostService;
+
 //	@RequestMapping(method = RequestMethod.GET)
 //	public ResponseEntity<List<Post>> findAll() {
 //		List<Post> list = PostService.findAll();
@@ -17,13 +24,13 @@ public class PostResource {
 //
 //		return ResponseEntity.ok().body(list);
 //	}
-//
-//	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
-//	public ResponseEntity<Post> findById(@PathVariable String id) {
-//		Post obj = PostService.findById(id);
-//
-//		return ResponseEntity.ok().body(new PostDTO(obj));
-//	}
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<Post> findById(@PathVariable String id) {
+		Post obj = PostService.findById(id);
+
+		return ResponseEntity.ok().body(obj);
+	}
 //
 //	@PostMapping
 //	public ResponseEntity<Post> insert(@RequestBody Post objDTO) {
